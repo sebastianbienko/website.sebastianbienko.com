c19dcc54356d2af041f14cc7ff22eca40d1bf279
Sebastian Bienko <net.sebastian.bienko@mhp.com>
2022-07-29 21:51:59 +0200

    corrected spelling

--- components/skills/skills.data.tsx
+++ components/skills/skills.data.tsx
@@ -1,390 +1,390 @@
 import { TileSkillData } from "./tile/tile.models";
 
 export const netSkillList: TileSkillData[] = [
-    {
-        value: "S.O.L.I.D.",
-        factor: 1,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.13em",
-            marginLeft: "-0.15em",
-            marginRight: 0,
-        },
+  {
+    value: "S.O.L.I.D.",
+    factor: 1,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.13em",
+      marginLeft: "-0.15em",
+      marginRight: 0,
     },
-    {
-        value: ".NET Core",
-        factor: 0.8,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.38em",
-            marginLeft: "-0.03em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: ".NET Core",
+    factor: 0.8,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.38em",
+      marginLeft: "-0.03em",
+      marginRight: 0,
     },
-    {
-        value: "ASP.NET MVC",
-        factor: 0.8,
-        margins: {
-            marginTop: "-0.07em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.22em",
-        },
+  },
+  {
+    value: "ASP.NET MVC",
+    factor: 0.8,
+    margins: {
+      marginTop: "-0.07em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.22em",
     },
-    {
-        value: "PowerShell",
-        factor: 0.5,
-        margins: {
-            marginTop: "-0.79em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.08em",
-        },
+  },
+  {
+    value: "PowerShell",
+    factor: 0.5,
+    margins: {
+      marginTop: "-0.79em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.08em",
     },
-    {
-        value: "Clean Code",
-        factor: 1.5,
-        margins: {
-            marginTop: "-0.28em",
-            marginBottom: 0,
-            marginLeft: "-0.52em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "Clean Code",
+    factor: 1.5,
+    margins: {
+      marginTop: "-0.28em",
+      marginBottom: 0,
+      marginLeft: "-0.52em",
+      marginRight: 0,
     },
-    {
-        value: "SQL",
-        factor: 0.8,
-        margins: {
-            marginTop: "-0.61em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.04em",
-        },
+  },
+  {
+    value: "SQL",
+    factor: 0.8,
+    margins: {
+      marginTop: "-0.61em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.04em",
     },
-    {
-        value: "MS SQL",
-        factor: 0.8,
-        margins: {
-            marginTop: "-0.19em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.20em",
-        },
+  },
+  {
+    value: "MS SQL",
+    factor: 0.8,
+    margins: {
+      marginTop: "-0.19em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.20em",
     },
-    {
-        value: "MondoDB",
-        factor: 0.8,
-        margins: {
-            marginTop: "-0.19em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.20em",
-        },
+  },
+  {
+    value: "MongoDB",
+    factor: 0.8,
+    margins: {
+      marginTop: "-0.19em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.20em",
     },
-    {
-        value: ".NET Framework",
-        factor: 0.8,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.04em",
-            marginLeft: "-0.27em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: ".NET Framework",
+    factor: 0.8,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.04em",
+      marginLeft: "-0.27em",
+      marginRight: 0,
     },
-    {
-        value: "C#",
-        factor: 2,
-        margins: {
-            marginTop: "-0.37em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.16em",
-        },
+  },
+  {
+    value: "C#",
+    factor: 2,
+    margins: {
+      marginTop: "-0.37em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.16em",
     },
-    {
-        value: "Solr",
-        factor: 0.7,
-        margins: {
-            marginTop: "-0.53em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.30em",
-        },
+  },
+  {
+    value: "Solr",
+    factor: 0.7,
+    margins: {
+      marginTop: "-0.53em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.30em",
     },
-    {
-        value: "Microservices",
-        factor: 1,
-        margins: {
-            marginTop: "-0.08em",
-            marginBottom: 0,
-            marginLeft: "-0.69em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "Microservices",
+    factor: 1,
+    margins: {
+      marginTop: "-0.08em",
+      marginBottom: 0,
+      marginLeft: "-0.69em",
+      marginRight: 0,
     },
+  },
 ];
 
 export const sitecoreSkillList: TileSkillData[] = [
-    {
-        value: "Sitecore XP",
-        factor: 1.4,
-        margins: {
-            marginTop: "-0.09em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.87em",
-        },
+  {
+    value: "Sitecore XP",
+    factor: 1.4,
+    margins: {
+      marginTop: "-0.09em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.87em",
     },
-    {
-        value: "Forms",
-        factor: 0.7,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.47em",
-            marginLeft: 0,
-            marginRight: "-0.44em",
-        },
+  },
+  {
+    value: "Forms",
+    factor: 0.7,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.47em",
+      marginLeft: 0,
+      marginRight: "-0.44em",
     },
-    {
-        value: "EXM",
-        factor: 1,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.92em",
-            marginLeft: "-0.66em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "EXM",
+    factor: 1,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.92em",
+      marginLeft: "-0.66em",
+      marginRight: 0,
     },
-    {
-        value: "Personalization",
-        factor: 0.8,
-        margins: {
-            marginTop: "-0.98em",
-            marginBottom: 0,
-            marginLeft: "-0.46em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "Personalization",
+    factor: 0.8,
+    margins: {
+      marginTop: "-0.98em",
+      marginBottom: 0,
+      marginLeft: "-0.46em",
+      marginRight: 0,
     },
-    {
-        value: "Engagement Automation",
-        factor: 0.6,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.33em",
-            marginLeft: 0,
-            marginRight: "-0.45em",
-        },
+  },
+  {
+    value: "Engagement Automation",
+    factor: 0.6,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.33em",
+      marginLeft: 0,
+      marginRight: "-0.45em",
     },
-    {
-        value: "Helix",
-        factor: 1.7,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.33em",
-            marginLeft: 0,
-            marginRight: "-0.52em",
-        },
+  },
+  {
+    value: "Helix",
+    factor: 1.7,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.33em",
+      marginLeft: 0,
+      marginRight: "-0.52em",
     },
-    {
-        value: "WFFM",
-        factor: 0.4,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.01em",
-            marginLeft: "-0.74em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "WFFM",
+    factor: 0.4,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.01em",
+      marginLeft: "-0.74em",
+      marginRight: 0,
     },
-    {
-        value: "SXA",
-        factor: 0.8,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.04em",
-            marginLeft: 0,
-            marginRight: "-0.63em",
-        },
+  },
+  {
+    value: "SXA",
+    factor: 0.8,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.04em",
+      marginLeft: 0,
+      marginRight: "-0.63em",
     },
-    {
-        value: "Commerce Connect",
-        factor: 0.6,
-        margins: {
-            marginTop: "-0.69em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.57em",
-        },
+  },
+  {
+    value: "Commerce Connect",
+    factor: 0.6,
+    margins: {
+      marginTop: "-0.69em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.57em",
     },
-    {
-        value: "xCommerce",
-        factor: 0.6,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.84em",
-            marginLeft: "-0.33em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "xCommerce",
+    factor: 0.6,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.84em",
+      marginLeft: "-0.33em",
+      marginRight: 0,
     },
-    {
-        value: "JSS",
-        factor: 1,
-        margins: {
-            marginTop: "-0.02em",
-            marginBottom: 0,
-            marginLeft: "-0.40em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "JSS",
+    factor: 1,
+    margins: {
+      marginTop: "-0.02em",
+      marginBottom: 0,
+      marginLeft: "-0.40em",
+      marginRight: 0,
     },
-    {
-        value: "xAnalytics",
-        factor: 0.8,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.09em",
-            marginLeft: "-0.78em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "xAnalytics",
+    factor: 0.8,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.09em",
+      marginLeft: "-0.78em",
+      marginRight: 0,
     },
-    {
-        value: "SPEAK",
-        factor: 0.6,
-        margins: {
-            marginTop: "-0.98em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.06em",
-        },
+  },
+  {
+    value: "SPEAK",
+    factor: 0.6,
+    margins: {
+      marginTop: "-0.98em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.06em",
     },
-    {
-        value: "Unicorn",
-        factor: 1,
-        margins: {
-            marginTop: "-0.50em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.30em",
-        },
+  },
+  {
+    value: "Unicorn",
+    factor: 1,
+    margins: {
+      marginTop: "-0.50em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.30em",
     },
-    {
-        value: "TDS",
-        factor: 1,
-        margins: {
-            marginTop: "-0.80em",
-            marginBottom: 0,
-            marginLeft: "-0.81em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "TDS",
+    factor: 1,
+    margins: {
+      marginTop: "-0.80em",
+      marginBottom: 0,
+      marginLeft: "-0.81em",
+      marginRight: 0,
     },
-    {
-        value: "Glass.Mapper.Sc",
-        factor: 1,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.77em",
-            marginLeft: 0,
-            marginRight: "-0.80em",
-        },
+  },
+  {
+    value: "Glass.Mapper.Sc",
+    factor: 1,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.77em",
+      marginLeft: 0,
+      marginRight: "-0.80em",
     },
+  },
 ];
 
 export const frontendSkillList: TileSkillData[] = [
-    {
-        value: "JavaScript",
-        factor: 1.4,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.27em",
-            marginLeft: 0,
-            marginRight: "-0.40em",
-        },
+  {
+    value: "JavaScript",
+    factor: 1.4,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.27em",
+      marginLeft: 0,
+      marginRight: "-0.40em",
     },
-    {
-        value: "HTML",
-        factor: 1,
-        margins: {
-            marginTop: "-0.23em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.42em",
-        },
+  },
+  {
+    value: "HTML",
+    factor: 1,
+    margins: {
+      marginTop: "-0.23em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.42em",
     },
-    {
-        value: "SASS",
-        factor: 0.7,
-        margins: {
-            marginTop: "-0.24em",
-            marginBottom: 0,
-            marginLeft: "-0.29em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "SASS",
+    factor: 0.7,
+    margins: {
+      marginTop: "-0.24em",
+      marginBottom: 0,
+      marginLeft: "-0.29em",
+      marginRight: 0,
     },
-    {
-        value: "gulp",
-        factor: 0.9,
-        margins: {
-            marginTop: "-0.86em",
-            marginBottom: 0,
-            marginLeft: 0,
-            marginRight: "-0.43em",
-        },
+  },
+  {
+    value: "gulp",
+    factor: 0.9,
+    margins: {
+      marginTop: "-0.86em",
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: "-0.43em",
     },
-    {
-        value: "webpack",
-        factor: 0.6,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.49em",
-            marginLeft: 0,
-            marginRight: "-0.88em",
-        },
+  },
+  {
+    value: "webpack",
+    factor: 0.6,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.49em",
+      marginLeft: 0,
+      marginRight: "-0.88em",
     },
-    {
-        value: "CSS",
-        factor: 1,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.76em",
-            marginLeft: 0,
-            marginRight: "-0.22em",
-        },
+  },
+  {
+    value: "CSS",
+    factor: 1,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.76em",
+      marginLeft: 0,
+      marginRight: "-0.22em",
     },
-    {
-        value: "React",
-        factor: 1.7,
-        margins: {
-            marginTop: "-0.03em",
-            marginBottom: 0,
-            marginLeft: "-0.51em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "React",
+    factor: 1.7,
+    margins: {
+      marginTop: "-0.03em",
+      marginBottom: 0,
+      marginLeft: "-0.51em",
+      marginRight: 0,
     },
-    {
-        value: "Vue",
-        factor: 0.6,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.92em",
-            marginLeft: 0,
-            marginRight: "-0.24em",
-        },
+  },
+  {
+    value: "Vue",
+    factor: 0.6,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.92em",
+      marginLeft: 0,
+      marginRight: "-0.24em",
     },
-    {
-        value: "Redux",
-        factor: 1.2,
-        margins: {
-            marginTop: 0,
-            marginBottom: "-0.65em",
-            marginLeft: 0,
-            marginRight: "-0.35em",
-        },
+  },
+  {
+    value: "Redux",
+    factor: 1.2,
+    margins: {
+      marginTop: 0,
+      marginBottom: "-0.65em",
+      marginLeft: 0,
+      marginRight: "-0.35em",
     },
-    {
-        value: "Angular",
-        factor: 0.6,
-        margins: {
-            marginTop: "-0.84em",
-            marginBottom: 0,
-            marginLeft: "-0.74em",
-            marginRight: 0,
-        },
+  },
+  {
+    value: "Angular",
+    factor: 0.6,
+    margins: {
+      marginTop: "-0.84em",
+      marginBottom: 0,
+      marginLeft: "-0.74em",
+      marginRight: 0,
     },
+  },
 ];
