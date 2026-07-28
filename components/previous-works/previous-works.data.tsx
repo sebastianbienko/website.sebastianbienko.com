4eb3dd70f0c9e4ea09490f1aec66e68003e05dd1
Sebastian Bienko <sebastianbienko@users.noreply.github.com>
2026-07-28 18:57:58 +0200

    content adjustments

--- components/previous-works/previous-works.data.tsx
+++ components/previous-works/previous-works.data.tsx
@@ -6,18 +6,19 @@ export const PreviousWorkData = [
     client: "MongoDB",
     roles: "consulting engineer",
     description: [
-      <>Designed and engineered a financial-sector lakehouse architecture, utilizing <code>Trino</code> and <code>Apache Airflow</code> to orchestrate scalable data pipelines for downstream analytics and <code>RAG</code> integrations.</>,
-      <>Developed a scalable, containerized <code>RESTful API</code> leveraging <code>AWS Lambda</code> and <code>Docker</code> to enable dynamic, high-performance data access and extend core database capabilities.</>,
+      <>Designed and engineered a financial-sector lakehouse architecture, utilizing <code>Trino</code>, <code>Apache Airflow</code>, <code>Apache Iceberg</code>, <code>Superset</code> and <code>MongoDB</code> to orchestrate scalable data pipelines for downstream analytics and <code>RAG</code> integrations.</>,
+      <>Developed a scalable, containerized <code>RESTful API</code> leveraging <code>AWS Lambda</code> and <code>Docker</code> to enable dynamic, high-performance data access and extend <code>MongoDB</code> core database capabilities.</>,
       <>Advised enterprise clients on <code>distributed database architecture</code>, executing <code>sharding strategies</code> and cluster scaling to optimize high-throughput, large-scale distributed deployments.</>
     ]
   },
   {
-    dates: { from: 2023, till: 2024 },
+    dates: { from: 2022, till: 2024 },
     client: "MHP / Porsche",
     roles: "tech lead / backend developer",
     description: [
-      <>The <code>Porsche.com</code> website plays a crucial role in the company&apos;s online marketing strategy, enabling users worldwide to explore their products.</>,
-      <>Therefore, it&apos;s essential for the site to maintain high availability and stay up to date. I initially joined the platform team, responsible for managing the foundation, as a backend developer. Later, I transitioned into the role of tech lead, where I improved team structure and processes, while putting a technical focus on enhancing system stability and headless functionalities.</>,
+      <>Led the platform team for <code>porsche.com</code> of 5 Software Engineers, 1 DevOps Engineer, and 1 QA Engineer. Established <code>deployment automation</code> best practices which reduced the deployment time by about 40%.</>,
+      <>Implemented scalable <code>REST API</code> and <code>GraphQL</code> interfaces and spearheaded the migration of a legacy monolithic architecture to <code>Jamstack</code> conform decoupled <code>microservices</code>, optimizing backend data aggregation and system performance.</>,
+      <>Spearheaded technical implementation and infrastructure readiness for high-traffic global product rollouts, ensuring maximum system stability and uptime.</>
     ],
   },
   {
@@ -25,8 +26,8 @@ export const PreviousWorkData = [
     client: "MHP / Porsche",
     roles: "tech lead / backend developer",
     description: [
-      <>Porsche Stories serves as a hub for emotionally driven content, engaging visitors with the products through various channels.</>,
-      <>As a backend developer, my primary focus was to expand content editing and headless capabilities. About a year later, I assumed additional responsibilities as tech lead. We successfully integrated a significant amount of content, delivering it to end users through our own decoupled frontend application and providing it to other teams through two distinct <code>APIs</code> tailored to specific use cases.</>,
+      <>Led an implementation team of 3 Software Engineers and 1 DevOps Engineer to build a centralized content hub.</>,
+      <>Developed two distinct content APIs based on <code>REST API</code> and <code>GraphQL</code> and integrated a decoupled frontend, managing the end-to-end backend architecture.</>,
     ],
   },
   {
@@ -34,17 +35,16 @@ export const PreviousWorkData = [
     client: "Deloitte / BMW",
     roles: "build manager / technical consultant",
     description: [
-      <>The BMW used car portal enables dealerships worldwide to advertise and sell used vehicles.</>,
-      <>As a build manager, my primary responsibility was to act as a liaison between the client&apos;s complex IT infrastructure and the external implementation team. In this role, I collaborated with the product owner to drive product improvements and requirements. Additionally, I provided technical expertise to assist the team during implementation.</>,
+      <>Handled <code>data integration</code> and <code>incident resolution</code> for the BMW Used Car Portal across a globally distributed, heterogeneous tech stack.</>,
+      <>Collaborated closely with cross-functional technical teams to manage critical incidents, driving rapid root-cause analysis and system resolutions.</>,
     ],
   },
   {
-    dates: { from: 2017, till: 2018 },
-    client: ".comspace / Nolte Küchen",
-    roles: "tech lead / project manager",
+    dates: { from: 2015, till: 2018 },
+    client: ".comspace",
+    roles: "backend developer",
     description: [
-      <>For many, the journey to their dream kitchen begins online. Nolte Küchen seamlessly guides users from their online kitchen plan to the nearest dealership.</>,
-      <>Early in my career, I had the opportunity to take on the dual role of tech lead and project manager. While challenging, this role provided ample opportunities for growth. We successfully integrated a kitchen planner into the user journey on the website and implemented various engagement mechanisms to assist users in taking the next step.</>,
+      <>In my first job as a backend developer, I had the opportunity to work on a wide variety of client projects and learn every aspect of the product release cycle. Some of the core clients I developed solutions for during this period included Dornbracht, Nolte Küchen, hkk Krankenkasse, Leopold Kostal, and swb AG.</>,
     ],
   },
 ];
